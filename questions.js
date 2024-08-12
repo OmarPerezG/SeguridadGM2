39231bca9d04229d8765d648ce6594f8705b525b
Omarpg <85317666+OmarPerezG@users.noreply.github.com>
2024-08-12 09:53:55 -0600

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,51 @@
+// questions.js
+const questions = [
+    {
+        question: "¿Cuáles son las 4 categorías que clasifica EPP de Arc Flash?",
+        options: ["4cal/cm2,  8cal/cm2,  25cal/cm2,  40cal/cm2", "4cal/cm2,  8cal/cm2,  25cal/cm2,  50cal/cm2", "4cal/cm2,  8cal/cm2,  30cal/cm2,  40cal/cm2", "4cal/cm2,  18cal/cm2,  25cal/cm2,  40cal/cm2"],
+        answer: 0
+    },
+    {
+        question: "¿Cuáles son los Riesgos Eléctricos?",
+        options: ["Cortadura Eléctrica y Arcamiento Eléctrico", "Choque Eléctrico y Arco Eléctrico", "Moldeadura Eléctrica y Insolación Eléctrica", "Trauma Eléctrica y Contusión Eléctrica"],
+        answer: 1
+    },
+    {
+        question: "¿Qué nivel de Voltaje se considera peligroso en General Motors?",
+        options: ["Después de 110 V.C.A.", "Después de 50 V.C.A.", "Después de 220 V.C.A.", "Después de 30 V.C.A."],
+        answer: 1
+    },
+    {
+        question: "¿Para qué nivel de voltaje está recomendada la herramienta dieléctrica?",
+        options: ["Para 220 Volts.", "Para 480 Volts.", "Para 1000 Volts.", "Para 500 Volts."],
+        answer: 2
+    },
+    {
+        question: "¿A qué distancia en 480 Volts puede generarse un arco eléctrico?",
+        options: ["A 10 pulgadas", "A 1 pulgada", "A 7 pulgadas", "A 10 centímetros"],
+        answer: 1
+    },
+    {
+        question: "¿Cuál es un dispositivo de control de energías peligrosas?",
+        options: ["Cortina de luz", "Botón de Paro de Emergencia", "Sensor de Presencia", "Todas las anteriores"],
+        answer: 3
+    },
+    {
+        question: "¿Cuáles de estas opciones son tipos de Energías que existen?",
+        options: ["Eléctrica y Hidraulica", "Neumatica y Vórticera", "Tetrónica y Gravedad", "Mecanica y Bio-simbiótica"],
+        answer: 0
+    },
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    {
+        question: "¿Cual es la razon para la realización cambio de disposiciones en GM?",
+        options: ["Accidentes", "Fatalidades", "Subestaciones", "Todas las anteriores"],
+        answer: 3
+    },
+    {
+        question: "¿Cual es la razon para la realización cambio de disposiciones en GM?",
+        options: ["Accidentes", "Fatalidades", "Subestaciones", "Todas las anteriores"],
+        answer: 3
+    },
+];
+
+export default questions;
